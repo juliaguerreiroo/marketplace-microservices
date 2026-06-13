09723985d31641d827d631011b8a044e101493fa
juliaguerreiroo <judalanorag@gmail.com>
2026-06-13 03:56:30 -0300

    Sprint 1: Adding discovery service (Eureka)

---
+++ discovery-service/src/test/java/com/julia/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.julia.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
